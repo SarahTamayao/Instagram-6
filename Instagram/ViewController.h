6aa3cef385db64202bf6b0cbfbc745a21762e326
smeng03 <68476325+smeng03@users.noreply.github.com>
2021-07-06 10:22:38 -0700

    Initial Commit

---
+++ Instagram/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  Instagram
+//
+//  Created by Sabrina P Meng on 7/6/21.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Instagram/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  Instagram
+//
+//  Created by Sabrina P Meng on 7/6/21.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
